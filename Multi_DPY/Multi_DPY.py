05f59218805e4955d748216f5dd1903b170cbca5
Jixxie <robertoeliascc@gmail.com>
2024-11-04 09:43:37 -0500

    CHANGES DONE:
    -ADDED IP ADDRESS FOR THE FG

--- Multi_DPY/Multi_DPY.py
+++ Multi_DPY/Multi_DPY.py
@@ -1,12 +1,21 @@
-#THIS IS A TEST
+#THIS IS A TEST  MULTI DEPLOY
 import time
 import paramiko
 
-# Replace with FortiGate details
-
-FORTIGATE_IP = "10.10.14.32"
-USERNAME = "admin"
-PASSWORD = "4gu4k4n_17"
+# Define FortiGate details as a list of dictionaries
+fortigate_devices = [
+    {
+        "ip": "10.10.14.32",
+        "username": "admin",
+        "password": "4gu4k4n_17"
+    },
+    {
+        "ip": "10.10.14.30",
+        "username": "admin",
+        "password": "4gu4k4n_17"
+    }
+    # Add more FortiGate devices as needed
+]
 
 def execute_commands(client, commands):
     """Execute a list of CLI commands on the FortiGate."""
@@ -18,24 +27,23 @@ def execute_commands(client, commands):
     output = shell.recv(65535).decode()
     print(output)
 
-def configure_fortigate():
+def configure_fortigate(device):
     """Connect to FortiGate and run configuration commands."""
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(FORTIGATE_IP, username=USERNAME, password=PASSWORD)
+    client.connect(device["ip"], username=device["username"], password=device["password"])
 
     # Commands for IPsec Tunnel (Step 1)
-    # Commands in this section might need to be adjusted just in the case the device is alredy running n' configured otherwhise leave default.
     commands_phase1 = [
         "config vpn ipsec phase1-interface",
         "edit AGK-OCI-CARCAMO",
         "set interface wwan",
         "set remote-gw 159.54.146.111",
-        "set psksecret 1iAoDhcEAgk2025#", #Static PSKY
+        "set psksecret 1iAoDhcEAgk2025#",  # Static PSK
         "set proposal 3des-md5",
         "set xauthtype client",
-        "set authusr CAC_LAKIN",  #User, depends of each Fortigate
-        "set authpasswd T3lemetria.2025#", #Static Password
+        "set authusr CAC_LAKIN",  # User, depends on each Fortigate
+        "set authpasswd T3lemetria.2025#",  # Static Password
         "set auto-negotiate enable",
         "set autokey-keepalive enable",
         "set keylife 3600",
@@ -105,7 +113,9 @@ def configure_fortigate():
     execute_commands(client, commands_policy2)
 
     client.close()
-    print("VPN configuration completed successfully.")
+    print(f"VPN configuration completed successfully for device {device['ip']}.")
 
 if __name__ == "__main__":
-    configure_fortigate()
+    # Loop through each FortiGate device and apply the configuration
+    for device in fortigate_devices:
+        configure_fortigate(device)
